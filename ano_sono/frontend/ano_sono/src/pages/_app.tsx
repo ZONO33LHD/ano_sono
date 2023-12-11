24aa538a0609126799a68b8e821491b82427efd3
ZONO33LHD <shunsuk.free@gmail.com>
2023-12-12 00:16:23 +0900

    :sweat_smile:何とかGoogle認証追加

--- ano_sono/frontend/ano_sono/src/pages/_app.tsx
+++ ano_sono/frontend/ano_sono/src/pages/_app.tsx
@@ -1,13 +1,13 @@
 // ano_sono/frontend/ano_sono/src/pages/_app.tsx
 import '../app/globals.css'
 import type { AppProps } from 'next/app'
-import NextAuthProvider from '../providers/NextAuth'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider>
+    <SessionProvider>
       <Component {...pageProps} />
-    </NextAuthProvider>
+    </SessionProvider>
   )
 }
 
